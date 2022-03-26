0662decc18ff699545b85b5be49c2300772a9910
Jarosław Sokolik <jaroslawsokolik@gmail.com>
2022-03-26 23:18:47 +0100

    builtins notes added

--- builtins/builtins.py
+++ builtins/builtins.py
@@ -1,60 +1,73 @@
-# abs()
-# A negative value’s absolute is that value is positive
+"""abs()
+A negative value absolute is that value is positive
+"""
 
 print(abs(-7))
 
-# all()
-# Function takes a container as an argument.
-# This Built in Functions returns True if all values in a python iterable have
-# a Boolean value of True
+"""all()
+Function takes a container as an argument.
+This Built in Functions returns True if all values in a python iterable have
+a Boolean value of True
+"""
 
 print(all({'*', '', ''}))
 
-# any()
-# Like all(), it takes one argument and returns True if, even one value
-# in the iterable has a Boolean value of True
+"""any()
+Like all(), it takes one argument and returns True if, even one value
+in the iterable has a Boolean value of True
+"""
 
 print(any((1, 0, 0)))
 
-# ascii()
+"""
+ascii()
 # Returns a printable representation of a python object
 # (like a string or a Python list)
+"""
 
 print(ascii('ș'))
 
-# bin()
-# Converts an integer to a binary string
+"""
+bin()
+Converts an integer to a binary string
+"""
 
 print(bin(7))
 
-# bool()
-# Converts a value to Boolean
+"""bool()
+Converts a value to Boolean
+"""
 
 print(bool(1))
 
-# bytes()
-# Returns an immutable bytes object
+"""bytes()
+Returns an immutable bytes object
+"""
 
 print(bytes(5))
 
-# bytearray()
-# Returns a python array of a given byte size
+"""bytearray()
+Returns a python array of a given byte size
+"""
 
 print(bytearray(4))
 
-# callable()
-# Tells us if an object can be called.
+"""callable()
+Tells us if an object can be called.
+"""
 
 print(callable(list))
 print(callable([1, 2, 3, 4]))
 
-# chr()
-# Returns the character in python for an ASCII value
+"""chr()
+Returns the character in python for an ASCII value
+"""
 
 print(chr(48))
 
-# classmethod()
-# Returns a class method for a given method.
+"""classmethod()
+Returns a class method for a given method.
+"""
 
 
 class fruit:
@@ -66,42 +79,44 @@ class fruit:
 fruit.sayhi = classmethod(fruit.sayhi)
 fruit.sayhi
 
-# compile()
-# Returns a Python code object. We use it to convert a string code into object
-# code
+"""compile()
+Returns a Python code object. We use it to convert a string code into object
+code
+"""
 
 exec(compile('a=5\nb=7\nprint(a+b)', '', 'exec'))
 
-# Here, ‘exec’ is the mode.
-# The parameter before that is the filename for the file form which the code
-# is read.Finally, we execute it using exec()
-
-# filter()
-# Filters out the items for which the condition is True.
+"""filter()
+Filters out the items for which the condition is True.
+"""
 
 print(list(filter(lambda x: x % 2 == 0, [1, 2, 0, False])))
 
-# float()
-# It converts an int or a compatible value into a float
+"""float()
+It converts an int or a compatible value into a float
+"""
 
 print(float(12))
 
-# format()
-# Function formats a specified value into a specified format
+"""format()
+Function formats a specified value into a specified format
+"""
 
 print(format(0.5, '%'))
 
-# Frozenset()
-# Freeze the list, and make it unchangeable
+"""Frozenset()
+Freeze the list, and make it unchangeable
+"""
 
 mylist = ['apple', 'banana', 'cherry']
 x = frozenset(mylist)
 x[1] = "strawberry"
 print(x)
 
-# getattr()
-# Function returns the value of the specified attribute from the specified 
-# object
+"""getattr()
+Function returns the value of the specified attribute from the specified
+object
+"""
 
 
 class Person:
@@ -113,6 +128,3 @@ class Person:
 x = getattr(Person, 'age')
 
 print(x)
-
-# globals()
-
