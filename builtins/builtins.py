27ceb155eab40ac316f658774533ba7bb749dcf5
Jarosław Sokolik <jaroslaw.sokolik@clicktrans.pl>
2022-03-28 16:35:50 +0200

    builtins python notes

--- builtins/builtins.py
+++ builtins/builtins.py
@@ -110,8 +110,6 @@ Freeze the list, and make it unchangeable
 
 mylist = ['apple', 'banana', 'cherry']
 x = frozenset(mylist)
-x[1] = "strawberry"
-print(x)
 
 """getattr()
 Function returns the value of the specified attribute from the specified
@@ -128,3 +126,128 @@ class Person:
 x = getattr(Person, 'age')
 
 print(x)
+
+"""globals()
+Function returns the global symbol table as a dictionary.
+A symbol table contains necessary information about the current program
+"""
+
+x = globals()
+print(x)
+
+"""hasattr()
+The hasattr() function returns True if the specified object has the specified attribute, otherwise False.
+"""
+
+class Person:
+    name = "John"
+    age = 36
+    country = "Norway"
+
+x = hasattr(Person, 'age')
+
+"""hash()
+To be completed
+"""
+
+"""help()
+Executes the built-in help system
+"""
+# help(print)
+
+"""help()
+Converts a number into a hexadecimal value
+"""
+
+x = hex(255)
+print(x)
+
+"""id()
+The id() function returns a unique id for the specified object.
+"""
+
+x = ('apple', 'banana', 'cherry')
+print(id(x))
+
+"""input()
+The input() function allows user input.
+"""
+
+print('Enter your name:')
+x = input()
+print('Hello, ' + x)
+
+"""int()
+The int() function converts the specified value into an integer number.
+"""
+
+print(int(3.5))
+
+"""isinstance()
+The isinstance() function returns True if the specified object is of the specified type, otherwise False.
+"""
+
+x = isinstance(5, int)
+print(x)
+
+"""issubclass()
+The issubclass() function returns True if the specified object is a subclass of the specified object, otherwise False.
+"""
+# To be done
+
+"""iter()
+Create an iterator object, and print the items
+"""
+
+x = iter(["apple", "banana", "cherry"])
+print(next(x))
+print(next(x))
+print(next(x))
+
+"""len()
+Return the number of items in a list
+"""
+
+print(len("string"))
+x = [1, 2, 3]
+print(len(x))
+
+"""list()
+Function creates a list object.
+A list object is a collection which is ordered and changeable.
+"""
+
+x = list(('apple', 'banana', 'cherry'))
+print(x)
+
+"""locals()
+Function returns the local symbol table as a dictionary.
+A symbol table contains necessary information about the current program
+"""
+
+x = locals()
+print(x)
+
+"""map()
+"""
+
+# To be completed
+
+"""max()
+Return the largest number"""
+
+x = [1, 3 ,6, 1234, 45666]
+print(max(x))
+
+"""memoryview()
+Create and print a memoryview object
+"""
+print(memoryview(b"Hello"))
+
+"""min()
+Returns the smallest item in an iterable
+"""
+
+x = [1, 3 ,6, 1234, 45666]
+print(min(x))
+
