46a78b412e56603f914432d745669e9c902e71fd
Jarosław Sokolik <jaroslaw.sokolik@clicktrans.pl>
2022-08-03 16:06:15 +0200

    roadmap.sh learning structure

--- dates/dates.py
+++
@@ -1,7 +0,0 @@
-from babel import dates
-import datetime
-from random import randint
-
-d = datetime.datetime.today() + datetime.timedelta(days=randint(1,30))
-form = dates.format_datetime(d, "EEEE d MMMM yyyy",locale='pl_PL')
-print(form)
